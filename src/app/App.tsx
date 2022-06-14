79039370a014dead1b913fceaea9a70bcd8d366b
pavel <shirokuiuna@mail.ru>
2022-06-14 11:16:27 +0300

    Добавил пример подгрузки отдельного чанка

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { ReactComponent as SvgSprite } from 'src/assets/sprites/_sprite.svg';
 import MainPage from 'src/components/pages/main-page/main-page';
-import TestPage from 'src/components/pages/test-page/test-page';
+
+const TestPage = lazy(() => import('src/components/pages/test-page/test-page'));
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<MainPage />} />
-          <Route path="test-page" element={<TestPage />} />
+          <Route
+            path="test-page"
+            element={
+              <Suspense fallback={<>...</>}>
+                <TestPage />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
       <SvgSprite className="svg-sprite" />
