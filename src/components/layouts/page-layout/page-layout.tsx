df87b3908d2330c0af31a0e22861e02a9d17494d
pavel <shirokuiuna@mail.ru>
2022-06-30 22:22:26 +0300

    Добавил пропс className

--- src/components/layouts/page-layout/page-layout.tsx
+++ src/components/layouts/page-layout/page-layout.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { PageLayoutProps } from 'src/components/layouts/page-layout/types';
 import './page-layout.scss';
 
-function PageLayout({ children, renderFooter = () => null }: PageLayoutProps) {
+function PageLayout({ children, className = '', renderFooter = () => null }: PageLayoutProps) {
   return (
-    <div className="page-layout">
+    <div className={`page-layout ${className}`.trim()}>
       <div className="page-layout__content">{children}</div>
       <div className="page-layout__footer-wrap">{renderFooter()}</div>
     </div>
--- src/components/layouts/page-layout/types.ts
+++ src/components/layouts/page-layout/types.ts
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from 'react';
 
 export type PageLayoutProps = PropsWithChildren<{
+  className?: string;
   renderFooter?: () => JSX.Element | null;
 }>;
